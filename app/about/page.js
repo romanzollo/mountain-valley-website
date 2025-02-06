8a201f8eb12acf42372e65fba242dd310c351c93
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-02-06 22:51:10 +0200

    creating a layout

--- app/about/page.js
+++ app/about/page.js
@@ -1,10 +1,6 @@
-import Navigation from '../components/Navigation';
-
 export default function Page() {
     return (
         <div>
-            <Navigation />
-
             <h1>About Mountain Valley</h1>
         </div>
     );
--- app/page.js
+++ app/page.js
@@ -1,12 +1,8 @@
 import Link from 'next/link';
 
-import Navigation from './components/Navigation';
-
 export default function Page() {
     return (
         <div>
-            <Navigation />
-
             <h1>Mountain Valley. Welcome to paradise!</h1>
 
             <Link href="/cabins">Explore luxury cabins</Link>
