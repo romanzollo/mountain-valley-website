f12e4be7c16217348140138d0c8baa6672d4f81d
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-02-07 23:10:17 +0200

    displaying a loading indicator

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,7 +2,6 @@ import Counter from '../components/Counter';
 
 export default async function Page() {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
-
     const data = await res.json();
 
     return (
