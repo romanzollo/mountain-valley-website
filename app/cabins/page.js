b017c39b8d0901719a6217351fd1243107cf5469
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-02-07 22:44:04 +0200

    fetching data in a Page

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,7 +1,17 @@
-export default function Page() {
+export default async function Page() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    const data = await res.json();
+
     return (
         <div>
             <h1>Cabins page</h1>
+
+            <ul>
+                {data.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
         </div>
     );
 }
