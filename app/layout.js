e98dc4a6065c704ced923c03012d8043d44a4f11
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-02-12 23:04:27 +0200

    organizing the initial project structure

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import Navigation from './components/Navigation';
-import Logo from './components/Logo';
+import Navigation from '@/app/_components/Navigation';
+import Logo from '@/app/_components/Logo';
 
 // объявляем метаданные
 export const metadata = {
