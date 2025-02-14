acff860e94bbe395a836af78974bfc70b6ddd275
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-02-14 23:18:38 +0200

    improved the Navigation and Root Layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,3 @@
-import Navigation from '@/app/_components/Navigation';
-import Logo from '@/app/_components/Logo';
-
 // импортируем шрифт
 import { Josefin_Sans } from 'next/font/google';
 // настраиваем шрифт
@@ -10,6 +7,7 @@ const josefin = Josefin_Sans({
 });
 
 import '@/app/_styles/globals.css';
+import Header from './_components/Header';
 
 // объявляем метаданные
 export const metadata = {
@@ -28,16 +26,13 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <body
                 // добавляем класс шрифта josefin.className для подключения шрифта
-                className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+                className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased`}
             >
-                <header>
-                    <Logo />
-                    <Navigation />
-                </header>
+                <Header />
 
-                <main>{children}</main>
-
-                <footer>Coopyright by Mountain Valley</footer>
+                <div className="flex-1 px-8 py-12">
+                    <main className="max-w-7xl mx-aut">{children}</main>
+                </div>
             </body>
         </html>
     );
