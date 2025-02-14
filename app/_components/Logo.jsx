3ca7e1296f32a9e9b77594dcbf7d8a7c94c23b3a
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-02-14 23:40:50 +0200

    optimizing images with Image component (Next.js)

--- app/_components/Logo.jsx
+++ app/_components/Logo.jsx
@@ -1,9 +1,27 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import logo from '@/public/logo.png';
 
 function Logo() {
     return (
         <Link href="/" className="flex items-center gap-4 z-10">
-            <img src="/logo.png" height="60" width="60" alt="Mountain Valley" />
+            {/* простой вариант использования Image */}
+            {/* <Image
+                src="/logo.png"
+                // при использовании компонента Image (Next.js) нужно указать пропсы height и width для предотвращения сдвига всего макета приложения
+                height="60"
+                width="60"
+                alt="Mountain Valley"
+            /> */}
+
+            {/* более продвинутый вариант использования Image */}
+            <Image
+                src={logo}
+                height="60"
+                width="60"
+                quality={100} // качество изображения
+                alt="Mountain Valley"
+            />
             <span className="text-xl font-semibold text-primary-100">
                 Mountain Valley
             </span>
