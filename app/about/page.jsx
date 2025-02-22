377439ab6af055e5ebb8865de4c0e80acfe523cf
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-02-22 12:28:53 +0200

    fixed the display of the number of cabins on the About page + ISR

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,12 +4,22 @@ import Image from 'next/image';
 import image1 from '@/public/about-1.jpg';
 import image2 from '@/public/about-2.jpg';
 
+import { getCabins } from '@/app/_lib/data-service';
+
+// задаем время обновления данных страницы и кэширования (ISR)
+export const revalidate = 86400; // 1 day
+
 // объявляем метаданные для текущей страницы
 export const metadata = {
     title: 'About',
 };
 
-export default function Page() {
+export default async function Page() {
+    // получаем данные о всех каютах
+    const cabins = await getCabins();
+    // вычисляем количество кают
+    const cabinsNums = cabins.length;
+
     return (
         <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
             <div className="col-span-3">
@@ -27,11 +37,11 @@ export default function Page() {
                         enjoying simple pleasures with family.
                     </p>
                     <p>
-                        Our 8 luxury cabins provide a cozy base, but the real
-                        freedom and peace you&apos;ll find in the surrounding
-                        mountains. Wander through lush forests, breathe in the
-                        fresh air, and watch the stars twinkle above from the
-                        warmth of a campfire or your hot tub.
+                        Our {cabinsNums} luxury cabins provide a cozy base, but
+                        the real freedom and peace you&apos;ll find in the
+                        surrounding mountains. Wander through lush forests,
+                        breathe in the fresh air, and watch the stars twinkle
+                        above from the warmth of a campfire or your hot tub.
                     </p>
                     <p>
                         This is where memorable moments are made, surrounded by
