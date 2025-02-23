62a1fa06d741d7b45c3471c9737753e17644585b
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-02-23 23:11:30 +0200

    highlighting current side navigation link

--- app/_components/SideNavigation.jsx
+++ app/_components/SideNavigation.jsx
@@ -1,8 +1,11 @@
+'use client';
+
 import {
     CalendarDaysIcon,
     HomeIcon,
     UserIcon,
 } from '@heroicons/react/24/solid';
+import { usePathname } from 'next/navigation';
 
 import SignOutButton from './SignOutButton';
 import Link from 'next/link';
@@ -27,6 +30,10 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+    // вычисляем текущий путь через встроенный хук usePathname
+    // '/account', '/account/reservations' или '/account/profile'
+    const pathname = usePathname();
+
     return (
         <nav className="border-r border-primary-900">
             <ul className="flex flex-col gap-2 h-full text-lg">
@@ -34,7 +41,13 @@ function SideNavigation() {
                 {navLinks.map((link) => (
                     <li key={link.name}>
                         <Link
-                            className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+                            className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 
+                                /* подсвечиваем активную ссылку */
+                                ${
+                                    pathname === link.href
+                                        ? 'bg-primary-900'
+                                        : ''
+                                }`}
                             href={link.href}
                         >
                             {link.icon}
