af834168ee0875066b353251f8c1dc5c732c4336
Roman Zlagodukhin <zloyandmad@yandex.ru>
2025-03-16 14:10:37 +0200

    displaying a loading indicator with useFormStatus hook

--- app/_components/UpdateProfileForm.jsx
+++ app/_components/UpdateProfileForm.jsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useState } from 'react';
+import { useFormStatus } from 'react-dom';
+
 import { updateGuest } from '@/app/_lib/actions';
 
 function UpdateProfileForm({ guest, children }) {
@@ -65,11 +67,25 @@ function UpdateProfileForm({ guest, children }) {
             </div>
 
             <div className="flex justify-end items-center gap-6">
-                <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-                    Update profile
-                </button>
+                <Button />
             </div>
         </form>
+    );
+}
+
+// переносим кнопку в отдельный компонент чтобы использовать хук useFormStatus так как он может использоваться только внутри формы
+// если бы весь компонент UpdateProfileForm был серверный то пришлось бы компонент Button переносить в отдельный файл и делать его клиентским компонентом
+function Button() {
+    // хук для возможности виуализации отправки формы
+    const { pending } = useFormStatus();
+
+    return (
+        <button
+            className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+            disabled={pending}
+        >
+            {pending ? 'Updating...' : 'Update profile'}
+        </button>
     );
 }
 
